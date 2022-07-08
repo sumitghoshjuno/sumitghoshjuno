8a03ff2beaa5ff9e45cd0e6d1a4c68d06bfcb42f
SUMIT GHOSH <sumitghsh98@gmail.com>
2022-07-08 18:48:07 +0530

    Changes have been committed.

---
+++ src/Methodspracticesimp.java
@@ -0,0 +1,13 @@
+
+public class Methodspracticesimp {
+	public int multipynumbers(int a,int b) {
+			int z=a*b;
+			return z;
+		}
+public static void main(String[] args) {
+		Methodspracticesimp b=new Methodspracticesimp();
+				int ans= b.multipynumbers(10,3);
+				System.out.println("Multipilcation is :"+ans);
+		
+	}
+}
